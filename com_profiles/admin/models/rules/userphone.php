51f89d62cf1408a0d7a1b99c83b6bdf30b339755
Septdir <septdir@gmail.com>
2018-03-31 11:38:28 +0300

    change version to 1.0.2

--- com_profiles/admin/models/rules/userphone.php
+++ com_profiles/admin/models/rules/userphone.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package    Profiles Component
- * @version    1.0.1
+ * @version    1.0.2
  * @author     Nerudas  - nerudas.ru
  * @copyright  Copyright (c) 2013 - 2018 Nerudas. All rights reserved.
  * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
@@ -17,24 +17,22 @@ use Joomla\CMS\Language\Text;
 class JFormRuleUserPhone extends JFormRule
 {
 	/**
-	 * Method to test if two fields have a value in order to use only one field.
-	 * To use this rule, the form
-	 * XML needs a validate attribute of logoutuniquefield and a field attribute
-	 * that is equal to the field to test against.
+	 * Method to test the value.
 	 *
-	 * @param   SimpleXMLElement $element   The SimpleXMLElement object representing the `<field>` tag for the form field object.
-	 * @param   mixed            $value     The form field value to validate.
-	 * @param   string           $group     The field name group control value. This acts as an array container for the field.
-	 *                                      For example if the field has name="foo" and the group value is set to "bar" then the
-	 *                                      full field name would end up being "bar[foo]".
-	 * @param   Registry         $input     An optional Registry object with the entire data set to validate against the entire form.
-	 * @param   JForm            $form      The form object for which the field is being tested.
+	 * @param   \SimpleXMLElement  $element  The SimpleXMLElement object representing the `<field>` tag for the form field object.
+	 * @param   mixed              $value    The form field value to validate.
+	 * @param   string             $group    The field name group control value. This acts as as an array container for the field.
+	 *                                       For example if the field has name="foo" and the group value is set to "bar" then the
+	 *                                       full field name would end up being "bar[foo]".
+	 * @param   Registry           $input    An optional Registry object with the entire data set to validate against the entire form.
+	 * @param   Form               $form     The form object for which the field is being tested.
 	 *
 	 * @return  boolean  True if the value is valid, false otherwise.
 	 *
-	 * @since 1.0.0
+	 * @since   11.1
+	 * @throws  \UnexpectedValueException if rule is invalid.
 	 */
-	public function test(SimpleXMLElement $element, $value, $group = null, Registry $input = null, JForm $form = null)
+	public function test(\SimpleXMLElement $element, $value, $group = null, Registry $input = null, Form $form = null)
 	{
 		$id = $input->get('id', 0);
 
