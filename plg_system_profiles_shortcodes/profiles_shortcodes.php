6be03879f863f165190c62e2c035871ad227f3c2
Septdir <septdir@gmail.com>
2018-09-24 11:49:00 +0300

    Change version to 1.3.0

--- plg_system_profiles_shortcodes/profiles_shortcodes.php
+++ plg_system_profiles_shortcodes/profiles_shortcodes.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package    System - Profiles Shortcodes Plugin
- * @version    1.2.1
+ * @version    1.3.0
  * @author     Nerudas  - nerudas.ru
  * @copyright  Copyright (c) 2013 - 2018 Nerudas. All rights reserved.
  * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
