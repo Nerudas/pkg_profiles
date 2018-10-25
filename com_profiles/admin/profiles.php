28db60a4c7cb4dcc1e8d70ec5bca87083f263334
Septdir <septdir@gmail.com>
2018-10-25 13:13:03 +0300

    Add plugin check in component

--- com_profiles/admin/profiles.php
+++ com_profiles/admin/profiles.php
@@ -15,6 +15,7 @@ use Joomla\CMS\Access\Exception\NotAllowed;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\MVC\Controller\BaseController;
 use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Plugin\PluginHelper;
 
 JLoader::register('ProfilesHelper', __DIR__ . '/helpers/profiles.php');
 
@@ -24,6 +25,11 @@ HTMLHelper::_('stylesheet', 'media/com_profiles/css/admin-general.min.css', arra
 if (!Factory::getUser()->authorise('core.manage', 'com_profiles'))
 {
 	throw new NotAllowed(Text::_('JERROR_ALERTNOAUTHOR'), 403);
+}
+
+if (empty(PluginHelper::getPlugin('system', 'profiles')))
+{
+	throw new Exception(Text::_('COM_PROFILES_ERROR_PLUGIN_NOT_ENABLED'), 404);
 }
 
 $controller = BaseController::getInstance('Profiles');
